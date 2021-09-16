99002a99d0ac7f997fd26a0e43c55ed9c5f5473e
Alejandro Jose Sanchez Gomez <sanchezgalejandroj@gmail.com>
2021-09-16 12:54:25 +0200

    changing the font of the text, and finalitation of the stopwatch features

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+const { log: c } = console;
+
 let idMainTimerAnimation = null;
 let idLapAnimation = null;
 let millisecondsPassed = 0;
 let startTime = 0;
-const timeLapContainer = [];
+let timeLapContainer = [];
 const lapsNodeList = document.getElementsByClassName("lap-container_lap");
 const mainTimeEl = document.querySelector("#mainTime");
 const lapContainerEl = document.querySelector("ul");
 
-const createLapContainer = (numberLapContent, timeLapContent) => {
+const createLapElement = (numberLapContent, timeLapContent) => {
   const lapEl = document.createElement("li");
   const numberLapEl = document.createElement("span");
   const timeLapEl = document.createElement("span");
+
   lapEl.classList.add("lap-container_lap");
   timeLapEl.classList.add("timer");
-  timeLapEl;
+
   numberLapEl.innerText = numberLapContent;
   timeLapEl.innerText = timeLapContent;
 
@@ -23,22 +26,71 @@ const createLapContainer = (numberLapContent, timeLapContent) => {
   return lapEl;
 };
 
-const renderLap = () => {
-  lapContainerEl.insertBefore(
-    createLapContainer(`lap ${lapsNodeList.length + 1}`, "00:00.00"),
-    lapContainerEl.firstChild
-  );
-  startTimer(lapContainerEl.firstChild.lastChild, false);
+const emptyLapsEraser = () => {
+  const emptyLapEl = document.querySelector(".lap-container__empty-lap");
+  if (emptyLapEl) {
+    emptyLapEl.remove();
+  }
 };
 
-function leftFillNum(num) {
+const findBiggerAndLowerTime = () => {
+  const lapstimesSorted = [...timeLapContainer].sort(
+    (a, b) => b.millisecondsPassed - a.millisecondsPassed
+  );
+
+  const { id: idBigestLap } = lapstimesSorted[0];
+  const { id: idLowestLap } = lapstimesSorted[lapstimesSorted.length - 1];
+  const position = lapsNodeList.length - 1;
+
+  return [
+    lapsNodeList[position - idBigestLap],
+    lapsNodeList[position - idLowestLap],
+  ];
+};
+
+const removeColorLetterClasses = () => {
+  const previusBiggestTime = document.querySelector(
+    ".lap-container__letter-red"
+  );
+  const previusLowestTime = document.querySelector(
+    ".lap-container__letter-green"
+  );
+  if (previusBiggestTime && previusLowestTime) {
+    previusBiggestTime.classList.remove("lap-container__letter-red");
+    previusLowestTime.classList.remove("lap-container__letter-green");
+  }
+};
+
+const paintLines = () => {
+  if (lapsNodeList.length > 2) {
+    const [BiggestTime, LowestTime] = findBiggerAndLowerTime();
+    BiggestTime.classList.add("lap-container__letter-red");
+    LowestTime.classList.add("lap-container__letter-green");
+  }
+};
+
+/*!!!!!Check this function with the team, ask about the destructuring, is it necessary? what would be the acceptable way to do it */
+const renderLap = () => {
+  removeColorLetterClasses();
+  emptyLapsEraser();
+  const lapNumber = lapsNodeList.length + 1;
+  const lapEl = createLapElement(`lap ${lapNumber}`, "00:00.00");
+  const { lastChild: lapTimeEl } = lapEl;
+
+  lapContainerEl.insertBefore(lapEl, lapContainerEl.firstChild);
+
+  paintLines();
+  startTimer(lapTimeEl, false);
+};
+
+function addLeftZero(num) {
   return num.toString().padStart(2, 0);
 }
-
+//!!Question about this function, line 49, would it be better to send it as an object instead of an array? or is the same?
 const timeFormatter = (timeInMilliseconds) => {
-  const minutes = leftFillNum(Math.trunc(timeInMilliseconds / 60000));
-  const seconds = leftFillNum(Math.trunc((timeInMilliseconds % 60000) / 1000));
-  const centiSeconds = leftFillNum(
+  const minutes = addLeftZero(Math.trunc(timeInMilliseconds / 60000));
+  const seconds = addLeftZero(Math.trunc((timeInMilliseconds % 60000) / 1000));
+  const centiSeconds = addLeftZero(
     Math.trunc(((timeInMilliseconds % 60000) % 1000) / 10)
   );
   return [minutes, seconds, centiSeconds];
@@ -67,7 +119,7 @@ const changeTime = (startTime, htmlElement, idAnimation) => {
 
 const startTimer = (htmlElement, isMainTime, idAnimation) => {
   if (millisecondsPassed && isMainTime) {
-    startTime = Date.now() + millisecondsPassed;
+    startTime = Date.now() - millisecondsPassed;
   } else {
     startTime = Date.now();
   }
@@ -81,7 +133,7 @@ const changeButtonText = (textValue, textClass, target) => {
 };
 
 const stopTimer = (idLap, idMain) => {
-  millisecondsPassed = startTime - Date.now();
+  millisecondsPassed = Date.now() - startTime;
 
   if (idMain) window.cancelAnimationFrame(idMain);
 
@@ -113,13 +165,41 @@ document
 document
   .querySelector("#leftButton")
   .addEventListener("mouseup", ({ target }) => {
+    if (target.className === "controllers__disabled") return null;
     if (target.innerHTML === "Lap") {
+      stopTimer(idLapAnimation);
+      timeLapContainer.push({
+        millisecondsPassed,
+        id: lapsNodeList.length - 1,
+      });
+      renderLap();
+    }
+    if (target.innerHTML === "Reset") {
+      changeButtonText("Lap", "disabled", leftButton);
+      startTime = 0;
+      timeLapContainer = [];
+      millisecondsPassed = 0;
+      mainTimeEl.innerHTML = "00:00.00";
+      lapContainerEl.innerHTML = `<li class="lap-container__empty-lap"></li>
+      <li class="lap-container__empty-lap"></li>
+      <li class="lap-container__empty-lap"></li>
+      <li class="lap-container__empty-lap"></li>
+      <li class="lap-container__empty-lap"></li>
+      <li class="lap-container__empty-lap"></li>`;
+    }
+  });
+
+/*   if (
+      target.innerHTML === "Lap" &&
+      target.className !== "controllers__disabled"
+    ) {
       stopTimer(idLapAnimation);
       renderLap();
     } else {
+      changeButtonText("Lap", "disabled", leftButton);
       startTime = 0;
       millisecondsPassed = 0;
       mainTimeEl.innerHTML = "00:00.00";
       lapContainerEl.innerHTML = "";
     }
-  });
+   */
